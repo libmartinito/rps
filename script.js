13ce92c8e6460578150b0a0e06287fcee37ff039
Lib <lib.martinito@pm.me>
2022-01-10 11:52:43 +0800

    Update game logic

--- script.js
+++ script.js
@@ -13,80 +13,97 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function playerPlay() {
-  return window.prompt("Rock, paper or scissors?");
-}
-
 function playRound(computerSelection, playerSelection) {
   let playerPick = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
   let computerPick = computerSelection;
+  let result = document.querySelector(".result");
+
   if (playerPick == "Rock") {
     switch(computerPick) {
       case "Rock":
-        winner = "None"
-        console.log("It's a draw.");
+        receivesPoints = "None"
+        result.textContent = "It's a draw.";
         break;
       case "Paper":
-        winner = "Computer";
-        console.log(`You lose! ${computerPick} beats ${playerPick}`);
+        receivesPoints = "Computer";
+        result.textContent = `Point for me! ${computerPick} beats ${playerPick}.`;
         break;
       case "Scissors":
-        winner = "Player";
-        console.log(`You win! ${playerPick} beats ${computerPick}`);
+        receivesPoints = "Player";
+        result.textContent = `Point for you! ${playerPick} beats ${computerPick}.`;
         break;
     }
   }
   if (playerPick == "Paper") {
     switch(computerPick) {
       case "Rock":
-        winner = "Player";
-        console.log(`You win! ${playerPick} beats ${computerPick}`);
+        receivesPoints = "Player";
+        result.textContent = `Point for you! ${playerPick} beats ${computerPick}.`;
         break;
       case "Paper":
-        winner = "None";
-        console.log("It's a draw.");
+        receivesPoints = "None";
+        result.textContent = "It's a draw.";
         break;
       case "Scissors":
-        winner = "Computer";
-        console.log(`You lose! ${computerPick} beats ${playerPick}`);
+        receivesPoints = "Computer";
+        result.textContent = `Point for me! ${computerPick} beats ${playerPick}.`;
         break;
     }
   }
   if (playerPick == "Scissors") {
     switch(computerPick) {
       case "Rock":
-        winner = "Computer";
-        console.log(`You lose! ${computerPick} beats ${playerPick}`);
+        receivesPoints = "Computer";
+        result.textContent = `Point for me! ${computerPick} beats ${playerPick}.`;
         break;
       case "Paper":
-        winner = "Player";
-        console.log(`You win! ${playerPick} beats ${computerPick}`);
+        receivesPoints = "Player";
+        result.textContent = `Point for you! ${playerPick} beats ${computerPick}.`;
         break;
       case "Scissors":
-        winner = "None";
-        console.log("It's a draw.");
+        receivesPoints = "None";
+        result.textContent = "It's a draw.";
         break;
     }
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let winner = "";
-  for (let round = 0; round < 5; round++) {
-    playRound(computerPlay(), playerPlay());
-    if (winner == "Player") {
-      playerScore++;
-    } else {
-      computerScore++;
-    }
-  }
-  if (playerScore > computerScore) {
-    return "Congratulations, you won!";
-  } else if (computerScore > playerScore) {
-    return "Better luck next time, you lost!"
-  } else {
-    return "It's a draw!"
+let plyrPoints = 0;
+let compPoints = 0;
+let receivesPoints = "";
+let winner = "";
+
+function updateScore() {
+  let plyrScore = document.querySelector(".plyr-score");
+  let compScore = document.querySelector(".comp-score");
+  if (receivesPoints == "Player") {
+    plyrPoints++;
+    plyrScore.textContent = `Player: ${plyrPoints}`;
+  } else if (receivesPoints == "Computer") {
+    compPoints++;
+    compScore.textContent = `Computer: ${compPoints}`;
   }
 }
+
+function updateWinner() {
+  if (plyrPoints == 5 || compPoints == 5) {
+    if (plyrPoints == 5) {
+      winner = "Player";
+      document.querySelector(".result").textContent = "Congratulations! You won!";
+    } else {
+      winner = "Computer";
+      document.querySelector(".result").textContent = "Better luck next time! You lose!";
+    }
+  }
+}
+
+const btns = document.querySelectorAll(".btn");
+btns.forEach(btn => btn.addEventListener("click", function(e) {
+  if (winner == "") {
+    playRound(computerPlay(),e.target.textContent);
+    updateScore();
+    updateWinner();
+  } else {
+    alert("Please refresh the page for another game.")
+  }
+}));
